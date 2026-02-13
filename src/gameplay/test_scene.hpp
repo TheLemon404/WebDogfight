a4695418937d8cf559ef5f49903acf8f133b3936
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2026-02-13 13:24:20 -0600

    cloud layer

--- src/gameplay/test_scene.hpp
+++ src/gameplay/test_scene.hpp
@@ -20,8 +20,8 @@ class TestScene {
         testScene.entities.push_back(std::make_shared<Aircraft>("FA-XX", "resources/aircraft/FA-XX.json"));
 
         CloudsVolume cloud = CloudsVolume();
-        cloud.transform.position.y = 10000.0f;
-        cloud.transform.scale = glm::vec3(5000.0f, 5000.0f, 5000.0f);
+        cloud.transform.position.y = 15000.0f;
+        cloud.transform.scale = glm::vec3(50000.0f, 5000.0f, 50000.0f);
         testScene.environment.cloudVolumes.emplace_back(cloud);
 
         std::shared_ptr<AircraftWidgetLayer> layer = std::make_shared<AircraftWidgetLayer>();
