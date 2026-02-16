9960476d8a25a5e2914265499d28fef5a4cbfe76
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2026-02-16 14:32:34 -0600

    changes to gForce calculations (can't commit to web-live until later)

--- src/gameplay/aircraft.cpp
+++ src/gameplay/aircraft.cpp
@@ -37,7 +37,7 @@
 
 #define GRAVITY 17000.0f
 #define DRAG_COEFFICIENT 50.0f
-#define GFORCE_COEFFICIENT 150.0f
+#define GFORCE_COEFFICIENT 1.5f
 #define GFORCE_BODY_THRESHOLD 7
 #define GFORCE_TRAIL_THRESHOLD 9
 
@@ -212,10 +212,9 @@ void Aircraft::Update() {
         glm::vec3 velocityChange = velocity - lastVelocity;
         float lateralAcceleration = glm::length(velocityChange - glm::dot(velocityChange, unrotatedForward) * unrotatedForward);
 
-        gForce = MathUtils::Lerp<float>(gForce, lateralAcceleration / (GFORCE_COEFFICIENT * Time::deltaTime), Time::deltaTime * 5.0f);
+        gForce = MathUtils::Lerp<float>(gForce, lateralAcceleration / GFORCE_COEFFICIENT, Time::deltaTime * 5.0f);
 
         lastVelocity = velocity;
-
         lastRotation = unrolledRotation;
     }
     {
