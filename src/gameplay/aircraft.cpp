f4d46e1cc5961042a766c204e529db5384320f2f
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2026-03-12 15:10:24 -0500

    geforce now operates out of 1.0

--- src/gameplay/aircraft.cpp
+++ src/gameplay/aircraft.cpp
@@ -40,7 +40,7 @@
 
 #define GRAVITY 17000.0f
 #define DRAG_COEFFICIENT 50.0f
-#define GFORCE_COEFFICIENT 1.5f
+#define GFORCE_COEFFICIENT 0.3f
 #define GFORCE_BODY_THRESHOLD 7
 #define GFORCE_TRAIL_THRESHOLD 9
 
@@ -216,7 +216,7 @@ void Aircraft::Update() {
         glm::vec3 velocityChange = velocity - lastVelocity;
         float lateralAcceleration = glm::length(velocityChange - glm::dot(velocityChange, unrotatedForward) * unrotatedForward);
 
-        gForce = MathUtils::Lerp<float>(gForce, lateralAcceleration / GFORCE_COEFFICIENT, Time::deltaTime * 5.0f);
+        gForce = MathUtils::Lerp<float>(gForce, (lateralAcceleration / GFORCE_COEFFICIENT) + 1.0f, Time::deltaTime * 5.0f);
 
         lastVelocity = velocity;
         lastRotation = unrolledRotation;
