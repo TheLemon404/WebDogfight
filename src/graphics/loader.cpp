6f225a4e9e2c89abf9ee70fef05a43c955bc4cdb
Michael Teschner <120539187+TheLemon404@users.noreply.github.com>
2026-05-18 22:46:13 -0500

    nearest filtering for LUT

--- src/graphics/loader.cpp
+++ src/graphics/loader.cpp
@@ -314,8 +314,8 @@ Texture3D Loader::LoadTexture3DFromFile(const char* resourcePath, int width, int
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     int tempWidth = 0;
     int tempHeight = 0;
